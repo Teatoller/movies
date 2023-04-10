7ba992531eaaf52b321f05cdfce9390c236e24ff
steven ennis <steven.otieno1@gmail.com>
2023-04-10 22:52:34 +0300

    ft(review model): add reviews model

--- src/main/java/com/ennis/movies/Review.java
+++ src/main/java/com/ennis/movies/Review.java
@@ -1,2 +1,22 @@
-package com.ennis.movies;public class Review {
+package com.ennis.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+    private LocalDateTime created;
+    private LocalDateTime updated;
 }

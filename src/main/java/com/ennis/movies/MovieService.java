8692daf9cc3d7225762b6470d91212e123cf67a4
stevenennis <steven.otieno1@gmail.com>
2023-04-11 10:08:31 +0300

    ft(movie): get movie by id
    - add get single movie service method
    - add get single movie controller method

--- src/main/java/com/ennis/movies/MovieService.java
+++ src/main/java/com/ennis/movies/MovieService.java
@@ -1,11 +1,13 @@
 package com.ennis.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -15,5 +17,9 @@ public class MovieService {
     public ResponseEntity<List<Movie>>  allMovies(){
         return new ResponseEntity<List<Movie>>(movieRepository.findAll(), HttpStatus.OK) ;
 
+    }
+
+    public Optional<Movie> singleMovie(ObjectId id){
+        return movieRepository.findById(id);
     }
 }
